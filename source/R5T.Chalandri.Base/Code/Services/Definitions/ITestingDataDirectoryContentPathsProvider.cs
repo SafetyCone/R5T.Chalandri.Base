dc920d6744a6e7658c26f7d22d05a2484b9a099f
MinexAutomation <minex.automation@gmail.com>
2020-05-10 23:39:07 -0700

    Added example Visual Studio project file paths.

--- source/R5T.Chalandri.Base/Code/Services/Definitions/ITestingDataDirectoryContentPathsProvider.cs
+++ source/R5T.Chalandri.Base/Code/Services/Definitions/ITestingDataDirectoryContentPathsProvider.cs
@@ -3,12 +3,20 @@
 
 namespace R5T.Chalandri
 {
+    /// <summary>
+    /// Provides the file paths of individual testing data files.
+    /// </summary>
     public interface ITestingDataDirectoryContentPathsProvider
     {
         string GetBasicTextFilePath();
 
+        // Visual Studio solution files.
         string GetNewVisualStudio2017SolutionFilePath();
         string GetNewVisualStudio2019SolutionFilePath();
         string GetExampleVisualStudioSolutionFilePath();
+
+        // Visual Studio Project files.
+        string GetExampleVisualStudioProjectFilePath01();
+        string GetExampleVisualStudioProjectFilePath02();
     }
 }
